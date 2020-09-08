bfd68b8ebef09e962c2fc8a231acfe233560b97b
yousef2213 <mandooyousef988@gmail.com>
2020-09-08 14:27:47 +0200

    editing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import './App.css';
-import Login from './component/Login';
-import Home from './Home/Home';
-import ErrorDefault from './Error/error';
-import RepoComponent from './component/repoComponent';
-import { useAuth0 } from '@auth0/auth0-react';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import Login from "./component/Login";
+import Home from "./Home/Home";
+import ErrorDefault from "./Error/error";
+import RepoComponent from "./component/repoComponent";
+import { useAuth0 } from "@auth0/auth0-react";
 
+const App = () => {
+  const { isAuthenticated } = useAuth0();
 
-const App = () =>{
-  const {
-    isAuthenticated
-  } = useAuth0();
-
-  if(isAuthenticated){
-    return(
+  if (isAuthenticated) {
+    return (
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/repo" component={RepoComponent} />
         <Route component={ErrorDefault} />
       </Switch>
-    )
+    );
   }
   return (
     <>
@@ -30,6 +27,6 @@ const App = () =>{
       </Switch>
     </>
   );
-}
+};
 
 export default App;
--- src/Home/Input.js
+++ src/Home/Input.js
@@ -1,39 +1,31 @@
-import React from 'react'
-import {GithubConsumer} from '../context/context';
-import {Link} from 'react-router-dom'
+import React from "react";
+import { GithubConsumer } from "../context/context";
+import { Link } from "react-router-dom";
 export default function input() {
-    return (
-        <GithubConsumer>
-            {value =>{
-                let {
-                    handelChange,
-                    handelClick
-                } =value;
-                 return(
-                    <div className="container pt-5 pb-4">
-                        <form className="search-component">
-                            <div className="input-group">
-                                <input 
-                                onChange={handelChange} 
-                                type="text" 
-                                className="form-control search" 
-                                placeholder="Github Username" 
-                                aria-label="Github Username" 
-                                aria-describedby="addon-wrapping" 
-                                />
-                            </div>
-                            <Link
-                                to={`repo`} 
-                                className="btn-search"
-                                onClick={handelClick}
-                            >
-                                Seacrh
-                            </Link>
-                        </form>
-                    </div>
-                 )
-            }}
-        </GithubConsumer>
-        
-    )
+  return (
+    <GithubConsumer>
+      {(value) => {
+        let { handelChange, handelSubmit, handelSearch } = value;
+        return (
+          <div className="container pt-5 pb-4">
+            <form className="search-component" onSubmit={handelSearch}>
+              <div className="input-group">
+                <input
+                  onChange={handelChange}
+                  type="text"
+                  className="form-control search"
+                  placeholder="Github Username"
+                  aria-label="Github Username"
+                  aria-describedby="addon-wrapping"
+                />
+              </div>
+              <Link to={`repo`} className="btn-search" onClick={handelSubmit}>
+                Seacrh
+              </Link>
+            </form>
+          </div>
+        );
+      }}
+    </GithubConsumer>
+  );
 }
